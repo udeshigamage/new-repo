599d7bd5e3d7bdd41890b370802cbae3f04bec1e
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2023-12-08 03:37:57 +0530

    Did a small change to check out the cloud run

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <div>Hello, world! And have a nice day</div>;
+  return <div>Hello, world! And have a nice day! Do a small change</div>;
 };
 
 export default App;
