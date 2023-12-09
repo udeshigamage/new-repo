ac8e7f15a3ac54710369fe7879eb57f7cf8ea53a
sCodeu <sachindu99u@gmail.com>
2023-12-10 01:12:55 +0530

    remove /users in base

--- src/components/http-common.jsx
+++ src/components/http-common.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:"https://jsonplaceholder.typicode.com/users",
+    baseURL:"https://jsonplaceholder.typicode.com",
     headers:{
         'Accept':'application/json',
         'Content-Type':'application/json'
