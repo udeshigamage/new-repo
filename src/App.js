ebac559004689ec2a0a39de467461c62a7301b48
sCodeu <sachindu99u@gmail.com>
2023-12-09 12:46:59 +0530

    commit add routes for list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import list from './components/list';
 
 const App = () => {
-  return <div>Hello, world! And have a nice day! Do a small change</div>;
+  return 
+  <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/list' element='{<list/>}'></Route>
+    </Routes>
+    </BrowserRouter>
+    
+
+  </div>;
 };
 
 export default App;
