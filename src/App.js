5e6add328575d3c69888aa8bf69321d9113b2080
sCodeu <sachindu99u@gmail.com>
2023-12-12 15:04:59 +0530

    modify_all

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React from 'react';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import List_ from './components/List_.jsx';
-import Test from './components/Test.jsx';
-import Show_Profile from './components/Show_Profile.jsx';
-import AddUser from './components/AddUser.jsx';
-
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List_ from "./components/List_.jsx";
+import Test from "./components/Test.jsx";
+import Show_Profile from "./components/Show_Profile.jsx";
+import AddUser from "./components/AddUser.jsx";
 
 const App = () => {
-  return(
-  <div>
-    
-    
-     <BrowserRouter>
-    <Routes>
-      <Route path='/list' element={<List_/>}/>
-      <Route path='/adduser' element={<AddUser/>}/>
-      <Route path='/' element={<Test/>}/>
-      <Route path='/view/:id' element={<Show_Profile/>}/>
-    </Routes>
-    </BrowserRouter> 
-    
-
-  </div>);
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/list" element={<List_ />} />
+          <Route path="/adduser" element={<AddUser />} />
+          <Route path="/" element={<Test />} />
+          <Route path="/view/:id" element={<Show_Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,19 +1,18 @@
 import httpCommon from "./http-common";
 
-class Services{
-    getUsers(){
-        return httpCommon.get('/users');
-    }
-    getUserById(id){
-        return httpCommon.get(`/users/${id}`);
-
-    }
-    updateUser(id,data){
-        return httpCommon.put(`/users/${id}`,data);
-    }
-    postUsers(data){
-        return httpCommon.post('/users',data);
-    }
+class Services {
+  getUsers() {
+    return httpCommon.get("/users");
+  }
+  getUserById(id) {
+    return httpCommon.get(`/users/${id}`);
+  }
+  updateUser(id, data) {
+    return httpCommon.put(`/users/${id}`, data);
+  }
+  postUsers(data) {
+    return httpCommon.post("/users", data);
+  }
 }
 
 export default new Services();
