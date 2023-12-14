289746fc5255013309f92b11bbddfb4052cbaf9f
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2023-12-14 23:01:16 +0530

    Added support for configuring layout container style and each layout item style from outside.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./Layout.css";
 
-export default function Layout({ children }) {
+export default function Layout({ children, style, itemStyles = undefined }) {
     return (
-        <div className="layout">
+        <div className="layout" style={style}>
             {
                 children.map((child, index) => {
                     return (
-                        <div key={index} className="layout-item">
+                        <div key={index} className="layout-item" style={itemStyles && itemStyles.name == child.name ? itemStyles.style : undefined}>
                             {child}
                         </div>
                     );
