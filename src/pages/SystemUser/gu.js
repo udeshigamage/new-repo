d2c9dae75b92fa4589bf782d6fa30bc416f14469
udeshi <udeshigamage92@gmail.com>
2023-12-26 17:14:28 +0530

    modified branch

--- src/pages/SystemUser/gu.js
+++ src/pages/SystemUser/gu.js
@@ -3,7 +3,8 @@ import React from "react";
 const gu = () => {
   return (
     <div>
-      <h1> hi we are hear </h1>
+      <h1> hi we are hear sound no way </h1>
+      <h2>hi this new branch</h2>
     </div>
   );
 };
